9368dedcd53fcb321b734839ff2003d3db9fe656
felipesaldias <felipesaldias@gmail.com>
2022-01-17 18:03:42 -0300

    0.1.2 add country parameter for top_mentions method

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pysophia2",
-    version="0.1.1",
+    version="0.1.2",
     description="Python library for news discourse analysis",
     long_description=README,
     long_description_content_type="text/markdown",
